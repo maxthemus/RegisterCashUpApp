c07b6979def696f736731d4ddd103d077243eb5c
maxthemus <max.somerville62@gmail.com>
2023-12-24 01:35:29 +1300

    Finished the navigation tab using react navigator

--- RegisterCashApp/App.js
+++ RegisterCashApp/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import RegisterView from './Components/RegisterView';
 import { useEffect, useRef, useState } from 'react';
 import Register from './Models/Register';
@@ -9,16 +9,33 @@ import TableHeader from './Components/View/TableHeader';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import RegisterBalanceScreen from './Components/Screens/RegisterBalanceScreen';
+import DefaultRegisterScreen from './Components/Screens/DefaultRegisterScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-const Stack = createNativeStackNavigator();
+const Stack = createBottomTabNavigator();
+//const Stack = createMaterialTopTabNavigator();
 
 export default function App() {
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={RegisterBalanceScreen} options={{ headerTitle: () => null}} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen
+            name="Home"
+            component={RegisterBalanceScreen}
+            options={{ headerTitle: () => null}}
+
+          />
+          <Stack.Screen
+            name="Test"
+            component={DefaultRegisterScreen}
+            options={{ headerTitle: () => null}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
